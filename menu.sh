55f2056a8039f561255dd6e53079345e372d3fc7
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-24 14:12:19 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/bash
 
-
+rm CD_menu.sh
+rm CD_menu.sh.1
+rm CD_menu.sh.2
+rm CD_menu.sh.3
+rm CD_menu.sh.4
+rm CD_menu.sh.5
+rm CD_menu.sh.6
+rm CD_menu.sh.7
+rm CD_menu.sh.8
+rm CD_menu.sh.9
+rm CD_menu.sh.10
+rm CD_menu.sh.11
+rm CD_menu.sh.12
 
 sudo apt-get update
 sudo apt-get install curl
