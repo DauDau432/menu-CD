23d2ad87f489b163e1e49c70c2092feac601ef1c
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-24 12:24:10 +0700

    Update CD-menu.sh

--- CD-menu.sh
+++ CD-menu.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/bash
-
-rm menucaidat.sh
-rm menucaidat.sh.1
-rm menucaidat.sh.2
-rm menucaidat.sh.3
-rm menucaidat.sh.4
-rm menucaidat.sh.5
-rm menucaidat.sh.6
-rm menucaidat.sh.7
-rm menucaidat.sh.8
-rm menucaidat.sh.9
-rm menucaidat.sh.10
-rm menucaidat.sh.11
-rm menucaidat.sh.12
-
-clear
-sudo apt-get update
-sudo apt-get install curl
-sudo apt-get install bash
+sudo su
+rm menu.sh.
+rm menu.sh.1
+rm menu.sh.2
+rm menu.sh.3
+rm menu.sh.4
+rm menu.sh.5
+rm menu.sh.6
+rm menu.sh.7
+rm menu.sh.8
+rm menu.sh.9
+rm menu.sh.10
+rm menu.sh.11
+rm menu.sh.12
 
 rm menu_x-ui.sh
 rm menu_x-ui.sh.1
@@ -33,10 +28,16 @@ rm menu_x-ui.sh.10
 rm menu_x-ui.sh.11
 rm menu_x-ui.sh.12
 
-wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu_x-ui.sh
+sudo apt-get update
+sudo apt-get install curl
+sudo apt-get install bash
+
+wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD-menu_x-ui.sh
 chmod +x menu_x-ui.sh
 
+wget https://raw.githubusercontent.com/sprov065/x-ui/master/install.sh
+
 clear
-wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/menucaidat.sh
-chmod +x menucaidat.sh
-./menucaidat.sh
+wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu.sh
+chmod +x menu.sh
+./menu.sh
