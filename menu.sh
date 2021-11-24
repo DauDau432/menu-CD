80267607a5e125361db69971d663234be8b69f42
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-24 15:47:29 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -19,6 +19,6 @@ sudo apt-get install curl
 sudo apt-get install bash
 
 clear
-bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu.sh) 
+wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu.sh
 chmod +x CD_menu.sh
 ./CD_menu.sh
