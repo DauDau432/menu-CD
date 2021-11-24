ac8c7e405279aef8b89a8efe7e44f12ed2e68726
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-24 14:54:37 +0700

    Create CD_menu_x-ui.sh

---
+++ CD_menu_x-ui.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+rm menu_x-ui.sh
+rm menu_x-ui.sh.1
+rm menu_x-ui.sh.2
+rm menu_x-ui.sh.3
+rm menu_x-ui.sh.4
+rm menu_x-ui.sh.5
+rm menu_x-ui.sh.6
+rm menu_x-ui.sh.7
+rm menu_x-ui.sh.8
+rm menu_x-ui.sh.9
+rm menu_x-ui.sh.10
+rm menu_x-ui.sh.11
+rm menu_x-ui.sh.12
+
+clear
+wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu_x-ui.sh
+chmod +x menu_x-ui.sh
+./menu_x-ui.sh
