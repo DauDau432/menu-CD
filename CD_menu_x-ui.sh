cc0d474bb8cd3c6a97a3e999516ecaea7ac9d92a
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-24 21:01:41 +0700

    Update CD_menu_x-ui.sh

--- CD_menu_x-ui.sh
+++ CD_menu_x-ui.sh
@@ -16,6 +16,6 @@ rm menu_x-ui.sh.12
 
 clear
 #tải menu x-ui
-wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu_x-ui.sh
+bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu_x-ui.sh)
 chmod +x menu_x-ui.sh
 bash menu_x-ui.sh
