cda5eefa25aa16296e7966d8b2ea4ad2a19a9e0e
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-24 14:42:19 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -14,20 +14,6 @@ rm CD_menu.sh.10
 rm CD_menu.sh.11
 rm CD_menu.sh.12
 
-rm menu_x-ui.sh
-rm menu_x-ui.sh.1
-rm menu_x-ui.sh.2
-rm menu_x-ui.sh.3
-rm menu_x-ui.sh.4
-rm menu_x-ui.sh.5
-rm menu_x-ui.sh.6
-rm menu_x-ui.sh.7
-rm menu_x-ui.sh.8
-rm menu_x-ui.sh.9
-rm menu_x-ui.sh.10
-rm menu_x-ui.sh.11
-rm menu_x-ui.sh.12
-
 sudo apt-get update
 sudo apt-get install curl
 sudo apt-get install bash
