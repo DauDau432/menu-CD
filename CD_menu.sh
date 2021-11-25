7df0244dd7daf0105ec7f4828ee8e1f4ca9b6217
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-25 12:20:06 +0700

    Update CD_menu.sh

--- CD_menu.sh
+++ CD_menu.sh
@@ -18,7 +18,8 @@ read so
 if [ $so -eq 1 ]
 then
         clear
-        ./CD_menu_x-ui.sh
+        bash <( curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu_x-ui.sh )
+        ./CD_menu.sh
         
 elif [ $so -eq 2 ]
 then
