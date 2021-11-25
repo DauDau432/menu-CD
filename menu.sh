97132e3800d030318af1934f7d7354512f37b028
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-25 12:20:58 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,8 +5,3 @@ clear
 wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu.sh 
 chmod +x CD_menu.sh
 ./CD_menu.sh
-
-clear
-#tai bo cai menu x-ui
-wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu_x-ui.sh
-chmod +x CD_menu_x-ui.sh 
