8637354267aad3d5b3836dabe6cbf34069a68e7d
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-25 11:17:34 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -2,6 +2,6 @@
 
 clear
 #tải menu chính
-bash <( curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu.sh )
+wget https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu.sh 
 chmod +x CD_menu.sh
 ./CD_menu.sh
