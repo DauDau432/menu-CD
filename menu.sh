22b0318c1cb67e18e4d80b6b0ce57da6d895e092
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-25 10:46:24 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/bash
 
-rm CD_menu.sh
-rm CD_menu.sh.1
-rm CD_menu.sh.2
-rm CD_menu.sh.3
-rm CD_menu.sh.4
-rm CD_menu.sh.5
-rm CD_menu.sh.6
-rm CD_menu.sh.7
-rm CD_menu.sh.8
-rm CD_menu.sh.9
-rm CD_menu.sh.10
-rm CD_menu.sh.11
-rm CD_menu.sh.12
-
 clear
 #tải menu chính
 bash <( curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/CD_menu.sh )
