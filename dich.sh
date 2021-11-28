a5209847ab3368e271f13d7bea55c1d611473935
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2021-11-28 08:58:12 +0700

    Delete dich.sh

--- dich.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-text="$(xsel -o)"
-translate="$(wget -U "Mozilla/5.0" -qO - "http://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=es&dt=t&q=$(echo $text | sed "s/[\"'<>]//g")" | sed "s/,,,0]],,.*//g" | awk -F'"' '{print $2, $6}')"
-echo "$translate" | xclip -selection clipboard
-notify-send --icon=info "$text" "$translate"
